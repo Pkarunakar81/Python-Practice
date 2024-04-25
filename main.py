e3edaebdd6e1f141a968a72a310244cde1e9328e
Pkarunakar81 <46920623+Pkarunakar81@users.noreply.github.com>
2024-04-25 07:13:58 +0530

    modified

---
+++ main.py
@@ -0,0 +1,11 @@
+
+print("Hello, world!")
+print(1 + 2)
+print(9 - 5)
+#Exponent	--	3 ** 2 = 9
+print(3 ** 2)
+# Create a variable called test_var and give it a value of 4+5
+test_var = 4 + 5
+
+# Print the value of test_var
+print(test_var)
